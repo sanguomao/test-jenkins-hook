f707eaebb1ff2485c6242705dd9aeb71a99d6638
mayc <san.guomao@163.com>
2019-10-28 20:12:58 +0800

    ade

--- vars/testShared.groovy
+++ vars/testShared.groovy
@@ -7,20 +7,73 @@ def warning(message) {
 		println(this.env.DOCKER_DEV_REGISTRY)
 }
 
-def sendDingTalk(status, message) {
-    if (status == "success") {
-        img="https://s2.ax1x.com/2019/10/17/KEFSWd.png"
-    } else if (status == "failure") {
-        img="https://s2.ax1x.com/2019/10/17/KEFCQI.png"
-    } else if (status == "pending") {
-        img="https://s2.ax1x.com/2019/10/17/KEFkef.gif"
-    } else {
-        img=""
+// def sendDingTalk(status, message) {
+//     if (status == "success") {
+//         img="https://s2.ax1x.com/2019/10/17/KEFSWd.png"
+//     } else if (status == "failure") {
+//         img="https://s2.ax1x.com/2019/10/17/KEFCQI.png"
+//     } else if (status == "pending") {
+//         img="https://s2.ax1x.com/2019/10/17/KEFkef.gif"
+//     } else {
+//         img=""
+//     }
+//     dingTalk([
+//         accessToken: "${env.DING_TALK_ENTRYPOINT}",
+//         jenkinsUrl: "${env.JENKINS_URL}",
+//         imageUrl: img,
+//         message: message,
+//     ])
+// }
+
+def call(String lang) {
+  pipeline {
+    agent any
+
+    environment {
+        DOCKER_DEV_REGISTRY="https://harbor.longguikeji.com"
     }
-    dingTalk([
-        accessToken: "${env.DING_TALK_ENTRYPOINT}",
-        jenkinsUrl: "${env.JENKINS_URL}",
-        imageUrl: img,
-        message: message,
-    ])
+
+    stages {
+        stage('Build') {
+            steps {
+                script {
+                    info 'hello'
+                    warning 'world'
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            script {
+                sendDingTalk("success", "构建成功")
+            }
+        }
+        failure {
+            script {
+                sendDingTalk("failure", "构建失败")
+            }
+        }
+    }
+	}
+
+}
+
+def sendDingTalk(status, message) {
+	if (status == "success") {
+			img="https://s2.ax1x.com/2019/10/17/KEFSWd.png"
+	} else if (status == "failure") {
+			img="https://s2.ax1x.com/2019/10/17/KEFCQI.png"
+	} else if (status == "pending") {
+			img="https://s2.ax1x.com/2019/10/17/KEFkef.gif"
+	} else {
+			img=""
+	}
+	dingTalk([
+			accessToken: "${env.DING_TALK_ENTRYPOINT}",
+			jenkinsUrl: "${env.JENKINS_URL}",
+			imageUrl: img,
+			message: message,
+	])
 }
