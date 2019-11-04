cab2e9004b5ecbb3f90ed5c90b10b6264c20e499
mayc <san.guomao@163.com>
2019-11-04 15:46:27 +0800

    t10

--- vars/testShared.groovy
+++ vars/testShared.groovy
@@ -1,6 +1,7 @@
 
 // env.docker_image_build_version = getDockerImageBuildVersion()
 // docker_image_build_version=getDockerImageBuildVersion()
+a="abc"
 
 String getShortCommitForBuild() {
 	  return sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
@@ -14,6 +15,7 @@ String getDOCKER_IMAGE_BUILD_VERSION() {
 
 def info(message) {
     echo "INFO: ${message}"
+		println(a)
 		// def docker_image_build_version="abc"
 		// println(env.docker_image_build_version)
 		println(DOCKER_IMAGE_BUILD_VERSION)
@@ -44,141 +46,4 @@ String getRepoName() {
 		println(params.PROD_VERSION)
 		println(repoName)
 		return repoName
-}
-
-// def sendDingTalk(status, message) {
-//     if (status == "success") {
-//         img="https://s2.ax1x.com/2019/10/17/KEFSWd.png"
-//     } else if (status == "failure") {
-//         img="https://s2.ax1x.com/2019/10/17/KEFCQI.png"
-//     } else if (status == "pending") {
-//         img="https://s2.ax1x.com/2019/10/17/KEFkef.gif"
-//     } else {
-//         img=""
-//     }
-//     dingTalk([
-//         accessToken: "${env.DING_TALK_ENTRYPOINT}",
-//         jenkinsUrl: "${env.JENKINS_URL}",
-//         imageUrl: img,
-//         message: message,
-//     ])
-// }
-
-def call(String buildType) {
-	if (buildType == 'build') {
-		pipeline {
-			agent any
-
-			environment {
-					DOCKER_DEV_REGISTRY="https://harbor.longguikeji.com"
-			}
-
-			stages {
-					stage('Build') {
-							steps {
-									script {
-											info 'hello'
-											warning 'world'
-									}
-							}
-					}
-			}
-
-			post {
-					success {
-							script {
-									sendDingTalk("success", "构建成功")
-							}
-					}
-					failure {
-							script {
-									sendDingTalk("failure", "构建失败")
-							}
-					}
-			}
-		}
-	} else if(buildType == 'status') {
-		pipeline {
-			agent any
-
-			environment {
-					DOCKER_DEV_REGISTRY="https://status.longguikeji.com"
-			}
-
-			stages {
-					stage('status') {
-							steps {
-									script {
-											info 'hello'
-											warning 'world'
-									}
-							}
-					}
-			}
-
-			post {
-					success {
-							script {
-									sendDingTalk("success", "status构建成功")
-							}
-					}
-					failure {
-							script {
-									sendDingTalk("failure", "status构建失败")
-							}
-					}
-			}
-		}
-
-	} else {
-		pipeline {
-			agent any
-
-			environment {
-					DOCKER_DEV_REGISTRY="https://release.longguikeji.com"
-			}
-
-			stages {
-					stage('release') {
-							steps {
-									script {
-											info 'hello'
-											warning 'world'
-									}
-							}
-					}
-			}
-
-			post {
-					success {
-							script {
-									sendDingTalk("success", "release构建成功")
-							}
-					}
-					failure {
-							script {
-									sendDingTalk("failure", "release构建失败")
-							}
-					}
-			}
-		}
-	}
-}
-
-def sendDingTalk(status, message) {
-	if (status == "success") {
-			img="https://s2.ax1x.com/2019/10/17/KEFSWd.png"
-	} else if (status == "failure") {
-			img="https://s2.ax1x.com/2019/10/17/KEFCQI.png"
-	} else if (status == "pending") {
-			img="https://s2.ax1x.com/2019/10/17/KEFkef.gif"
-	} else {
-			img=""
-	}
-	dingTalk([
-			accessToken: "${env.DING_TALK_ENTRYPOINT}",
-			jenkinsUrl: "${env.JENKINS_URL}",
-			imageUrl: img,
-			message: message,
-	])
 }
